d63fc986529ab5bbd1ceb2d9922f0a8caab72b56
Bulrush3 <mike10.09.2000yandex.ru>
2024-04-15 02:00:03 +0500

    data ver2

--- lab4/data_upload.py
+++ lab4/data_upload.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-# Загрузка данных из файла titanic.csv
-titanic_df = pd.read_csv('datasets/titanic.csv')
-# Выбор нужных столбцов для нового датасета
-new_df = titanic_df[['Pclass', 'Sex', 'Age']]
-# Сохранение нового датасета в файл
-new_df.to_csv('datasets/dataset_3clmns.csv', index=False)
+## Загрузка данных из файла titanic.csv
+#titanic_df = pd.read_csv('datasets/titanic.csv')
+## Выбор нужных столбцов для нового датасета
+#new_df = titanic_df[['Pclass', 'Sex', 'Age']]
+## Сохранение нового датасета в файл
+#new_df.to_csv('datasets/dataset_3clmns.csv', index=False)
+df = pd.read_csv('datasets/dataset_3clmns.csv')
+mean_age = df['Age'].mean()
+df['Age'].fillna(mean_age, inplace=True)
+
+df.to_csv('datasets/dataset_3clmns.csv', index=False)
