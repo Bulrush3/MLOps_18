fe888bbb266c77e15884a2e018dd61b4fb187b46
Bulrush3 <mike10.09.2000yandex.ru>
2024-04-15 02:22:44 +0500

    data ver3

--- lab4/data_upload.py
+++ lab4/data_upload.py
@@ -7,7 +7,10 @@ import pandas as pd
 ## Сохранение нового датасета в файл
 #new_df.to_csv('datasets/dataset_3clmns.csv', index=False)
 df = pd.read_csv('datasets/dataset_3clmns.csv')
-mean_age = df['Age'].mean()
-df['Age'].fillna(mean_age, inplace=True)
+#mean_age = df['Age'].mean()
+#df['Age'].fillna(mean_age, inplace=True)
+sex_one_hot = pd.get_dummies(df['Sex'])
+df = pd.concat([df, sex_one_hot], axis=1)
+df = df.drop('Sex', axis=1)
 
 df.to_csv('datasets/dataset_3clmns.csv', index=False)
